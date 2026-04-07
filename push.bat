38baa4912107923bcb248f6857b148fc5d8d74f9
Nikhil <nikhilgupt7880@gmail.com>
2026-04-07 21:38:11 +0530

    Initial Release

--- push.bat
+++ push.bat
@@ -5,4 +5,4 @@ git commit -m "Initial Release"
 git branch -M main
 git remote remove origin
 git remote add origin https://github.com/nikhilgupta7880/neon-Expression-Evaluation-using-Stack-visualizer.git
-git push -u origin main
+git push -f -u origin main
